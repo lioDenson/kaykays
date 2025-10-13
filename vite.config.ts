d1ec39adc8ddb82a96635ba887a454e9efd31458
KIMGITO <kimanthidennis02@gmail.com>
2025-10-13 16:43:44 +0300

    vite config: basebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base:'',
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
@@ -13,7 +12,7 @@ export default defineConfig({
             refresh: true
         }),
         react(),
-        tailwindcss(),
+        tailwindcss()
         // wayfinder({ formVariants: true })
     ],
     esbuild: {
