f68b68f2272d49c7c49326cf6716610b52e38ec2
KIMGITO <kimanthidennis02@gmail.com>
2025-10-22 12:53:24 +0300

    show sale item on click on sale invoice number on sales table

--- resources/js/pages/Transactions/Sales/Index.tsx
+++ resources/js/pages/Transactions/Sales/Index.tsx
@@ -1,19 +1,20 @@
 import CustomIndexPage from '@/components/custom/custom-index-page';
-import CustomPagination from '@/components/custom/custom-pagination';
 import CustomToaster from '@/components/custom/custom-toaster';
+import { Badge } from '@/components/ui/badge';
 import AppLayout from '@/layouts/app-layout';
 import { ColumnDefinition } from '@/types/app-types';
 import { Head, router, usePage } from '@inertiajs/react';
 import { route } from 'ziggy-js';
 import { SaleInterface } from './sale-interfaces';
-import { Badge } from '@/components/ui/badge';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
 export default function Index({ sales }: { sales: SaleInterface }) {
     const { flash } = usePage().props as any;
     const handleCreateSale = () => {
         router.get(route('sales.create'));
     };
-    console.log(sales);
+
+    
 
     const saleColumns: ColumnDefinition<any>[] = [
         {
@@ -21,18 +22,36 @@ export default function Index({ sales }: { sales: SaleInterface }) {
             accessorFn: (row) => row.invoice_number,
             id: 'invoice_number',
             cell: (row) => {
-                return <span className="text-blue-600 hover:underline"> {row.invoice_number}</span>;
+                console.log(row.sale_items);
+                return (
+                    <Popover>
+                        <PopoverTrigger>
+                            <span className="text-blue-600 hover:cursor-pointer p-0.5"> {row.invoice_number}</span>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <p className="text-sm font-bold">Sale Items</p>
+
+                            {row.sale_items.map((item, i) => {
+                                return (
+                                    <p className="text-xs md:text-sm">
+                                        {i + 1}. {item.product.name} - {item.quantity}
+                                        {item.product.unit}
+                                    </p>
+                                );
+                            })}
+                        </PopoverContent>
+                    </Popover>
+                );
             },
             filterable: true,
-            sortable:true,
-            
+            sortable: true
         },
         {
             header: 'Customer',
-            accessorFn: (row) => row.customer?.user?.name || 'Unknown'
-            , cell: (row) => {
+            accessorFn: (row) => row.customer?.user?.name || 'Unknown',
+            cell: (row) => {
                 if (row.customer == null) {
-                    return <Badge variant={'danger'}>{ 'Not Found'}</Badge>;
+                    return <Badge variant={'danger'}>{'Not Found'}</Badge>;
                 }
                 return row.customer?.user?.name;
             }
@@ -44,7 +63,7 @@ export default function Index({ sales }: { sales: SaleInterface }) {
         {
             header: 'Amount',
             accessorKey: 'total',
-            sortable:true,
+            sortable: true
         },
         {
             header: 'Status',
@@ -61,7 +80,7 @@ export default function Index({ sales }: { sales: SaleInterface }) {
                     return <Badge variant={'danger'}>{row.status}</Badge>;
                 }
             },
-            sortable:true,
+            sortable: true
         },
         {
             header: 'Delivery',
@@ -95,17 +114,15 @@ export default function Index({ sales }: { sales: SaleInterface }) {
                     }
                 }}
                 Columns={saleColumns}
-                paginate={
-                    {
-                        from: sales.from,
-                        to: sales.to,
-                        total: sales.total,
-                        links: sales.links,
-                        next_page_url: sales.next_page_url,
-                        prev_page_url: sales.prev_page_url,
-                        current_page: sales.current_page
-                    }
-                }
+                paginate={{
+                    from: sales.from,
+                    to: sales.to,
+                    total: sales.total,
+                    links: sales.links,
+                    next_page_url: sales.next_page_url,
+                    prev_page_url: sales.prev_page_url,
+                    current_page: sales.current_page
+                }}
             />
         </AppLayout>
     );
